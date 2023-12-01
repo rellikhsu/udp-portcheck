52ff3651d15134f7769e30db63393f956afc4a6c
rellik <vm6jo4xjp6@gmail.com>
2023-12-01 19:24:31 +0800

    Update tcp_udp_checker.py
    update some regex match and encoding with utf-8

--- tcp_udp_checker.py
+++ tcp_udp_checker.py
@@ -1,18 +1,21 @@
+#!/usr/bin/python3
 import threading
 import socket
 import subprocess
 import time
 import re
 
+port_regex_template = r"udp\s+\d+\s+\d+\s+.*:{}\s"
+
 def is_udp_port_open(port, output):
-    """ Check if the specified UDP port is open using a modified regex """
-    regex = r"^udp\s.*:{}\b".format(port)
-    return bool(re.search(regex, output, re.MULTILINE))
+    """ Check if the specified UDP port is open """
+    regex = re.compile(port_regex_template.format(port))
+    return bool(regex.search(output))
 
 def get_netstat_output():
     """ Run the netstat command and return its output """
     result = subprocess.run(['netstat', '-anu'], stdout=subprocess.PIPE)
-    return result.stdout.decode()
+    return result.stdout.decode('utf-8')
 
 def tcp_server(port):
     """ Start a simple server on the specified TCP port """
@@ -22,32 +25,32 @@ def tcp_server(port):
             with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
                 try:
                     s.bind(("", port))
-                    s.listen()
+                    s.listen(20) #
                     print(f"TCP port {port} is listening...")
 
-                    while is_udp_port_open(port, get_netstat_output()):
-                        s.settimeout(1)  # Set a timeout for accepting connections
+                    while True:
+                        if not is_udp_port_open(port, get_netstat_output()):
+                            print(f"UDP port {port} closed, stopping TCP server...")
+                                                        break
+                        s.settimeout(1)
                         try:
                             conn, addr = s.accept()
                             with conn:
                                 print(f"Connection from {addr}")
                                 conn.sendall(b"Hello, TCP!\n")
                         except socket.timeout:
-                            continue  # Continue checking if the UDP port is open
+                            continue
                     print(f"TCP port {port} closing...")
-
-                except Exception as e:
-                    print(f"Error: {e}")
-                    time.sleep(1)  # Wait for a while and retry
+                except socket.error as e:
+                    print(f"Socket error: {e}")
+                    time.sleep(1)
         else:
             print(f"UDP port {port} is not available, waiting...")
-            time.sleep(1)  # Wait for a while before retrying
+            time.sleep(1)
 
 if __name__ == "__main__":
-    # Starting TCP servers for ports 500 and 4500 in separate threads
     threading.Thread(target=tcp_server, args=(500,), daemon=True).start()
     threading.Thread(target=tcp_server, args=(4500,), daemon=True).start()
 
-    # Keep the main thread alive
     while True:
         time.sleep(1)
