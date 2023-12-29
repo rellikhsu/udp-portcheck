dba192d9dbaa7829cfd5664ede115e0ca2e4657a
Rellik <rellik.hsu@gaiatechs.com>
2023-12-29 11:39:29 +0800

    - add daemonInstall and systemd

---
+++ daemonInstall.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+git clone https://github.com/rellikhsu/udp-portcheck.git
+chmod 755 udp-portcheck/tcp_udp_checker.py
+chown root:root udp-portcheck/tcp_udp_checker.py
+cp  udp-portcheck/tcp_udp_checker.py /usr/local/bin/
+cp udp-portcheck/tcp_udp_checker.service /etc/systemd/system/tcp_udp_checker.service
+systemctl enable tcp_udp_checker.service
+systemctl daemon-reload
