3cd94dc468adf52777deba0d02b9da5a3471e05b
Rellik <rellik.hsu@gaiatechs.com>
2023-12-30 23:48:52 +0800

    - add tcp connection close

--- tcp_udp_checker.py
+++ tcp_udp_checker.py
@@ -25,23 +25,39 @@ def check_udp_port(port):
 
 def tcp_server(port):
     """開啟或關閉對應的 TCP 伺服器，反映 UDP 端口的狀態。"""
+    server_socket = None
     while True:
         time.sleep(1)  # 每秒檢查一次 UDP 端口狀態
         if udp_ports_status[port]:  # 如果 UDP 端口開啟，則啟動 TCP 伺服器
-            if port not in tcp_server_sockets:
+            if not server_socket:
                 # 創建並存儲 socket 實例
                 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
                 server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
                 server_socket.bind(('', port))
-                server_socket.listen()
+                server_socket.listen(5)
                 tcp_server_sockets[port] = server_socket
                 logging.info(f"TCP 伺服器正在監聽端口 {port}。")
+            else:
+                # 接受連接
+                try:
+                    client_socket, addr = server_socket.accept()
+                    logging.info(f"來自 {addr} 的連接已接受。")
+                    client_socket.settimeout(10.0)  # 為連接設置超時
+                    # 在這裡添加處理客戶端請求的代碼
+                    # ...
+                    # 處理完成後，關閉連接
+                    client_socket.close()
+                    logging.info(f"來自 {addr} 的連接已關閉。")
+                except socket.timeout:
+                    logging.warning("連接超時。")
+                except Exception as e:
+                    logging.exception("處理連接時發生錯誤。")
         else:  # 如果 UDP 端口關閉，則關閉 TCP 伺服器
-            if port in tcp_server_sockets:
-                # 關閉並刪除 socket 實例
-                tcp_server_sockets[port].close()
-                del tcp_server_sockets[port]
+            if server_socket:
+                server_socket.close()
                 logging.info(f"TCP 伺服器端口 {port} 已關閉。")
+                server_socket = None
+                tcp_server_sockets.pop(port, None)
 
 def monitor_udp_ports():
     """監控 UDP 端口的狀態。"""
@@ -65,4 +81,6 @@ if __name__ == '__main__':
             time.sleep(10)
     except KeyboardInterrupt:
         logging.info("程序被手動中止。")
+        for port, server in tcp_server_sockets.items():
+            server.close()
 
